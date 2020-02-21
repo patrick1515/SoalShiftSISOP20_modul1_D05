3e5ad7af949e73a8f4a29aed2bfc736d57e35d3e
patrick1515 <61314890+patrick1515@users.noreply.github.com>
2020-02-21 18:06:04 +0700

    Create soal1.sh

---
+++ soal1.sh
@@ -0,0 +1,11 @@
+echo "soal 1a"
+ 
+awk -F "\t" '{x[$13] += $21}END{for (a in x) {print x[a], a}}' /home/imanuel/Downloads/Sample-Superstore.tsv | sort -rg | tail -2 | head -1
+ 
+ 
+echo "soal 1b"
+awk -F "\t" '/Central/{x[$11] += $21}END{for (a in x) {print x[a], a}}' /home/imanuel/Downloads/Sample-Superstore.tsv | sort -rg | tail -2
+ 
+echo "soal 1c"
+ 
+awk -F "\t" '{if($11="Texas"||$11="Illinois")}/Central/ {x[$17] += $21}END{for (a in x) {print x[a], a}}' /home/imanuel/Downloads/Sample-Superstore.tsv | sort -rg | tail -10
