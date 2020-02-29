356109f839171e892baf62123306ef85fa98a97e
patrick1515 <61314890+patrick1515@users.noreply.github.com>
2020-02-29 21:36:30 +0700

    Delete soal2_dekripsi.sh

--- soal2_dekripsi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-up=(ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ)
-low=(abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz)
-now=$(stat -c %y $1|grep -oP '(?<=[^ ] ).*(?=:.*:)')
-
-back=$(echo "${1%.txt}"|tr ${low:$now:26}${up:$now:26} ${low:0:26}${up:0:26})
-mv $1 $back".txt"
