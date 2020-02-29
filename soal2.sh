c809c174ef56bbe4f2e6483bab0a3270c70ab1e0
patrick1515 <61314890+patrick1515@users.noreply.github.com>
2020-02-29 18:17:26 +0700

    Revisi

--- soal2.sh
+++ soal2.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-char=(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
-for ((i = 1; i <= 28 ; i++)) do
-        rand=$[$RANDOM%${#char[@]}]
-        password="${password}${char[$rand]}"
-done
+x=$(echo $1|grep -oP  '.*(?=\.txt)')
+if [[ $x =~ ^[a-zA-Z]+$ ]]
+then
+        char=(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q$        for ((i = 1; i <= 28 ; i++)) do
+                rand=$[$RANDOM%${#char[@]}]
+                password="${password}${char[$rand]}"
+        done
+        echo $password>$1
+else
+        echo "Error"
+fi
